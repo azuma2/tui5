933274c8f2568be550dfe2c972d2fb4058adb5fd
azuma2 <kafuka1192@gmail.com>
2022-04-25 20:11:25 +0900

    hisabisa9

--- .history/database/migrations/2014_10_12_000000_create_users_table_20220425185613.php
+++ .history/database/migrations/2014_10_12_000000_create_users_table_20220425185613.php
@@ -21,7 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
         });
     }
 
