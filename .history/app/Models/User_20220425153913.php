9cf7d24740bb7ed24d0e55c6719238674087aadd
azuma2 <kafuka1192@gmail.com>
2022-04-25 17:17:21 +0900

    hisabisa8

--- .history/app/Models/User_20220425153913.php
+++ .history/app/Models/User_20220425153913.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name' => 'required|max:20',
+        'email' => 'email:rfc,dns',
+        'password' => 'min:6',
     ];
 
     /**
